12cee306e9f576cb4c1c8d3eec849d1a565f011e
An Tran Minh <tranminhan.my@gmail.com>
2015-12-05 22:44:42 +0700

    fix sort link

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -1,11 +1,2 @@
 $( document ).ready(function() {
-  $("button#price-up").click(function() {
-    window.location.href = window.location.pathname + "?sort=price+asc"
-  });
-  $("button#price-down").click(function() {
-    window.location.href = window.location.pathname + "?sort=price+desc"
-  });
-  $("button#name-az").click(function() {
-    window.location.href = window.location.pathname + "?sort=name+asc"
-  });
 });
