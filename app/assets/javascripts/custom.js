a40728d45653547acb5493849a92d929a0f285ca
An Tran Minh <tranminhan.my@gmail.com>
2015-12-07 00:05:55 +0700

    show review on section

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -1,2 +1,6 @@
-$( document ).ready(function() {
-});
+// $(document).on('page:load', function() {
+//   var $input = $('input.rating'), count = Object.keys($input).length;
+//   if (count > 0) {
+//       $input.rating();
+//   }
+// });
--- app/models/menu_item.rb
+++ app/models/menu_item.rb
@@ -1,3 +1,8 @@
 class MenuItem < ActiveRecord::Base
   has_many :reviews
+
+  def avg_review
+    0 if self.reviews.empty?
+    self.reviews.collect(&:star).sum.to_f/self.reviews.length
+  end 
 end
